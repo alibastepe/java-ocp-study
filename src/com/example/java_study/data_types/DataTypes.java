5c6447889228da5c03cf3b60b0b18b26e5c32cf8
Ali Baştepe <alibastepe@gmail.com>
2023-02-24 16:09:12 +0300

    DataTypes and StringStudy classes are added.

---
+++ src/com/example/java_study/data_types/DataTypes.java
@@ -0,0 +1,60 @@
+package com.example.java_study.data_types;
+
+public class DataTypes {
+    char c = 'a';
+    byte b = 0b1;
+    short s = 123; //2 bytes
+    int i = 2_345;  //4 bytes
+    long l = 12L; //8 bytes
+    float f = 12.3f;  //4 bytes
+    double d = 2345.9; //8 bytes
+
+    //underscore limitations
+    int octalValue = 0_413;
+    long hexVal = 0x_10_BA_75;  //invalid. cannot follow 0x
+    byte aByte = 0b_1; //invalid. cannot follow 0b
+    long binVal = 0b1_0100_10_01;  //valid
+    long aLong = 1231242_L;  //invalid. cannot precede L
+
+    int i2 = 45_;  //invalid cannot be at the beginning or end
+
+    double aDouble = 345._5;  //invalid. cannot be adjacent to decimal point
+    float aFloat = 52.4_f;  //invalid cannot be prior to f,F,D or d
+
+    //char
+    char c1 = 122;
+    char c2 = '\uffff';  //from \u0000 to \uffff (65535)
+    char c3 = -122;  //invalid
+    char c4 = (char)-122;
+
+
+    //var
+    var g = 13;  //invalid. can only be used in local variables. can NOt be used in method parameters or return types
+
+
+    static {
+        var f = 54;
+    }
+
+    {
+        var t = "ali";
+    }
+
+    public DataTypes()  {
+        final var j = 12.4;  // final is allowed with var
+        final int fd = 8;
+        try {
+            Thread.sleep(1000);
+        } catch (Exception f)  {
+            var n = 4;
+        }
+
+        var v1 = 10L + 0b1_001;   // v1 is long
+        var v2 = 12.4 + 23.5f;  //v2 is double
+
+        var charArr[] = {'a', 'b'};  //invalid. new char[] must be added
+
+    }
+
+
+}
---
+++ src/com/example/java_study/string/StringStudy.java
@@ -0,0 +1,90 @@
+
+package com.example.java_study.string;
+
+public class StringStudy {
+    //up to java 8, string impl. used char[]
+    //since Java9 byte[] is used to store characters and an encoding field is used.
+    //If all characters are ISO-8859-1/Latin-1, they use one byte per character
+
+
+
+    void stringPoolMethod()  {
+        //String pooling
+        String str1 = "myStr";
+        String str2 = "myStr";
+
+        System.out.println(str1 == str2); // true
+
+        //not pooled
+        String str3 = new String("ali");
+        String str4 = new String("ali");
+
+        System.out.println(str3 == str4);  //false
+        System.out.println(str3.equals(str4));  //true
+    }
+
+    void stringAPI()  {
+        String str = "abcdefghijklm";
+
+        System.out.println(str.charAt(3));  // d
+        str.indexOf('f');  // 5
+        str.indexOf("cd");  // 2
+        str.indexOf("cvf");  // -1
+
+        str.substring(3); //  defghijklm
+        str.substring(2,5); //cde    index 5 is exclusive
+
+        str.length();  // 13
+
+
+        String str2 = "    vbg  sdff   ";
+        str2.trim(); //vbg  sdff        leading and trailing spaces are removed.
+
+        //values returned by string methods are creaetd by "new" operator. So They are not pooled
+    }
+
+    void stringConcat()  {
+        String eJava = "eJava";
+        String guru = "Guru";
+        String concatStr = eJava + guru;
+        String eJavaGuru = "eJavaGuru";
+        //Strings computed by concatenation at runtime are newly created and distinct.
+        //String values created from concat. of constant exp. are computed at runtime and are treated as if they were literals
+        //if eJava and guru were FINAL, then below expression would be true
+        System.out.println(concatStr == eJavaGuru);  //false.
+
+
+        // + and += operations are evaluted from left to right
+        int num = 10;
+        int val = 12;
+        String aString = "str";
+
+        System.out.println(num + val + aString);  //prints 22str
+        System.out.println("" + num + val + aString);  //prints 1012str
+
+    }
+
+    void stringBuilder()  {
+        //StringBuilder stores a MUTABLE sequence of characters
+        //similar to String class, uses the concept of CompactStrings
+
+        StringBuilder sb = new StringBuilder(); //init. capacity 16
+        sb = new StringBuilder(56);
+        sb = new StringBuilder("myStr");   //underlying byte[] is created with bigger capacity
+
+        //charAt, indexOf, substring and length methods are same as in String class
+
+        sb.append("dfsddf");  //  myStrdfsddf
+        sb.append("wsdfred", 2, 4);  // myStrdfsddfdf
+
+        sb.insert(2, "ddf"); // myddfStrdfsddfdf
+
+        sb.delete(3, 6); //  mydtrdfsddfdf
+
+        //no trim method
+
+        sb.replace(4,7,"nnn");  //  mydtnnnsddfdf
+    }
+
+
+}
