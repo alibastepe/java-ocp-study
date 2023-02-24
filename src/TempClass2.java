843a42eb3f1f1f62abdea43a426f51f4e9c50e66
Ali Baştepe <alibastepe@gmail.com>
2023-02-24 14:28:30 +0300

    Initial folder commit

---
+++ src/TempClass2.java
@@ -0,0 +1,3 @@
+public class TempClass2 {
+    TempClass c = new TempClass();  //classes in default package can see each other
+}
---
+++ src/com/example/java_study/objects_and_classes/Main.java
@@ -0,0 +1,27 @@
+//package name convention:
+// all in lowercase,
+// seperated by dot,
+// can contain letters, digits, underscore
+package com.example.java_study.objects_and_classes;
+
+//import tempClass;  //ERROR default package classes cannot be imported from a named packaged class
+import static java.util.stream.Collectors.*;
+//  import java.awt.List;  //ERROR importing classes with same name
+
+
+public class Main {
+    public static void main(String[] args)  {
+        toList();  //imported statically from Collectors
+
+    }
+
+    //public-static changeable. This is also valid
+   /* static public void main(String... args)  {
+
+    }*/
+
+    //NOT runnable main method because signature is different
+    public static void main(String args)  {
+
+    }
+}
